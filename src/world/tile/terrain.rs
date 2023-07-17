3a0acb5937ed35be928f1b78b54e8603e1f5cdb6
Jackson Oliver <applesthepi@gmail.com>
2023-07-17 00:49:22 -0400

    terrain tile changes

--- src/world/tile/terrain.rs
+++ src/world/tile/terrain.rs
@@ -1,16 +1,12 @@
-use nalgebra::{Vector3, vector};
-
 use crate::ModTerrain;
 
 pub struct TileTerrain {
-	pub color: Vector3<f32>,
 	pub mod_terrain: Option<Box<dyn ModTerrain>>,
 }
 
 impl Default for TileTerrain {
 	fn default() -> Self {
 		Self {
-			color: vector![1.0, 1.0, 1.0],
 			mod_terrain: None,
 		}
 	}
