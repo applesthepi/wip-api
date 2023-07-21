afbabf9b4a03c321d87e16914764e9aa3939fd8f
Jackson Oliver <applesthepi@gmail.com>
2023-07-21 02:01:09 -0400

    fixed mod terrain

--- src/registry/mod_terrain.rs
+++ src/registry/mod_terrain.rs
@@ -2,9 +2,6 @@ use nalgebra::Vector3;
 
 pub struct ModTerrainReg {
 	pub color: Vector3<f32>,
-	/// If the item is terrain bound, then it can
-	/// not exist with a structure or building.
-	pub terrain_bound: bool,
 }
 
 pub trait ModTerrain {
