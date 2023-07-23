cbb763525e0d0cc57b4fd3b375950750a66f9c1f
Jackson Oliver <applesthepi@gmail.com>
2023-07-23 02:46:01 -0400

    tile_item update

--- src/world/tile/item.rs
+++ src/world/tile/item.rs
@@ -1,14 +1,18 @@
+use nalgebra::{vector, Vector3};
+
 use crate::ModItem;
 
 #[repr(C)]
 pub struct TileItem {
-	pub mod_item: Option<Box<dyn ModItem>>,
+	// pub mod_item: Option<Box<dyn ModItem>>,
+	pub color: Vector3<f32>,
 }
 
 impl Default for TileItem {
 	fn default() -> Self {
 		Self {
-			mod_item: None,
+			// mod_item: None,
+			color: vector![1.0, 1.0, 1.0],
 		}
 	}
 }
