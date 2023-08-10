490d3f094299bf0e2825e61c0a0f56d4958b90df
Jackson Oliver <applesthepi@gmail.com>
2023-08-10 03:38:33 -0400

    noise update

--- src/generator/chunk_generator/protocol/noise.rs
+++ src/generator/chunk_generator/protocol/noise.rs
@@ -6,10 +6,11 @@ use core::fmt::Debug;
 
 #[derive(Clone)]
 pub struct ProtocolNoise {
-	// 0.0 is level zero, 1.0 is level five.
-	pub height: Arc<NoiseMap>,
-	// <0.5 is none, >0.5 is some.
-	pub valid: Arc<NoiseMap>,
+	/// (
+	/// 	HEIGHT: 0.0 is level zero, 1.0 is level five.
+	/// 	VALID:  <0.5 is none, >0.5 is some.
+	/// )
+	pub maps: Vec<(Arc<NoiseMap>, Arc<NoiseMap>)>,
 }
 
 impl Debug for ProtocolNoise {
