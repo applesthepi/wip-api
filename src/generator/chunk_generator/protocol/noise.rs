c5f844c572add80f3d23867060bc8008261708fa
Jackson Oliver <applesthepi@gmail.com>
2023-08-17 18:12:36 -0400

    noise proxy

--- src/generator/chunk_generator/protocol/noise.rs
+++ src/generator/chunk_generator/protocol/noise.rs
@@ -1,8 +1,45 @@
 use std::sync::Arc;
 
-use noise::utils::NoiseMap;
+use noise::{utils::NoiseMap, TranslatePoint, NoiseFn};
 
 use core::fmt::Debug;
+
+use crate::PT_MOD_WCOUNT;
+
+pub trait NoiseProxy {
+	fn get(
+		&self,
+		abs_chunk_position: [i32; 2],
+		rel_position: [usize; 2],
+	) -> f64;
+}
+
+pub struct NoiseContainer<N: NoiseFn<f64, 2>> {
+	pub noise: N,
+}
+
+impl<N: NoiseFn<f64, 2>> NoiseContainer<N> {
+	pub fn new(
+		noise: N,
+	) -> Self {
+		Self {
+			noise,
+		}
+	}
+}
+
+impl<N: NoiseFn<f64, 2>> NoiseProxy for NoiseContainer<N> {
+	fn get(
+		&self,
+		abs_chunk_position: [i32; 2],
+		rel_position: [usize; 2],
+	) -> f64 {
+		let translation = TranslatePoint::new(&self.noise)
+			.set_x_translation((abs_chunk_position[0] * (PT_MOD_WCOUNT as i32)) as f64)
+			.set_y_translation((abs_chunk_position[1] * (PT_MOD_WCOUNT as i32)) as f64);
+		translation.get([rel_position[0] as f64, rel_position[1] as f64])
+	}
+}
 
 #[derive(Clone)]
 pub struct ProtocolNoise {
@@ -10,7 +47,7 @@ pub struct ProtocolNoise {
 	/// 	HEIGHT: 0.0 is level zero, 1.0 is level five.
 	/// 	VALID:  <0.5 is none, >0.5 is some.
 	/// )
-	pub maps: Vec<(Arc<NoiseMap>, Arc<NoiseMap>)>,
+	pub maps: Vec<(Arc<dyn NoiseProxy>, Arc<dyn NoiseProxy>)>,
 }
 
 impl Debug for ProtocolNoise {
