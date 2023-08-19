a15e8f3374de4a02deb4fb319cbc08fc378e679e
Jackson Oliver <applesthepi@gmail.com>
2023-08-19 15:51:34 -0400

    private tile fields and WTOperation

--- src/world/tile.rs
+++ src/world/tile.rs
@@ -16,22 +16,26 @@ pub use covor::*;
 
 use crate::{PT_MOD_WCOUNT, RTTerrain, RTFloor, RTRoof, RTStructure, RTItem, RTCover, RTBuilding};
 
+pub enum WTOperation {
+	Success,
+	ObjectInWay,
+	OutOfSpace,
+}
+
 // TODO: covor (like filth/rubble etc.)
 pub struct WorldTile {
-	pub terrain: [Option<RTTerrain>; 4],
-	pub floor: Option<RTFloor>,
-	pub roof: Option<RTRoof>,
-	pub building: [Option<RTBuilding>; 16],
-	pub structure: Option<RTStructure>,
-	pub cover: [Option<RTCover>; 16],
-	pub item: Option<RTItem>,
+	terrain: [Option<RTTerrain>; 4],
+	item: Option<RTItem>,
+	building: [Option<RTBuilding>; 16],
+	structure: Option<RTStructure>,
+	roof: Option<RTRoof>,
+	cover: [Option<RTCover>; 16],
 }
 
 impl Default for WorldTile {
 	fn default() -> Self {
 		Self {
 			terrain: [(); 4].map(|_| None),
-			floor: None,
 			roof: None,
 			building: [(); 16].map(|_| None),
 			structure: None,
@@ -56,29 +60,112 @@ impl WorldTile {
 		Some(self.terrain.len() - 1)
 	}
 
-	pub fn set_high_cover(
+	pub fn get_item(
+		&self,
+	) -> Option<RTItem> {
+		self.item.clone()
+	}
+
+	pub fn get_buildings(
+		&self,
+	) -> [Option<RTBuilding>; 16] {
+		self.building.clone()
+	}
+
+	pub fn get_structure(
+		&self,
+	) -> Option<RTStructure> {
+		self.structure.clone()
+	}
+
+	pub fn get_roof(
+		&self,
+	) -> Option<RTRoof> {
+		self.roof.clone()
+	}
+
+	pub fn get_cover(
+		&self,
+	) -> [Option<RTCover>; 16] {
+		self.cover.clone()
+	}
+
+	pub fn set_terrain(
 		&mut self,
-		rt: Option<RTCover>,
-	) -> bool {
-		for x in self.cover.iter_mut() {
-			if let None = x {
-				*x = rt;
-				return true;
-			}
+		rt: Option<RTTerrain>,
+		height: u8,
+	) {
+		assert!(height < self.terrain.len() as u8);
+		self.terrain[height as usize] = rt;
+	}
+
+	pub fn set_item(
+		&mut self,
+		rt: Option<RTItem>,
+	) -> WTOperation {
+		if let Some(_) = &self.structure {
+			return WTOperation::ObjectInWay;
 		}
-		false
+		self.item = rt;
+		WTOperation::Success
 	}
 
 	pub fn set_high_building(
 		&mut self,
 		rt: Option<RTBuilding>,
-	) -> bool {
+	) -> WTOperation {
+		if let Some(_) = &self.structure {
+			return WTOperation::ObjectInWay;
+		}
 		for x in self.building.iter_mut() {
 			if let None = x {
 				*x = rt;
-				return true;
+				return WTOperation::Success;
 			}
 		}
-		false
+		WTOperation::OutOfSpace
+	}
+
+	pub fn set_structure(
+		&mut self,
+		rt: Option<RTStructure>,
+	) -> WTOperation {
+		if let Some(_) = &self.item {
+			return WTOperation::ObjectInWay;
+		}
+		for building in self.building.iter() {
+			if let Some(_) = &building {
+				return WTOperation::ObjectInWay;
+			}
+		}
+		for cover in self.cover.iter_mut() {
+			*cover = None;
+		}
+		self.structure = rt;
+		WTOperation::Success
+	}
+
+	pub fn set_roof(
+		&mut self,
+		rt: Option<RTRoof>,
+	) -> WTOperation {
+		self.roof = rt;
+		WTOperation::Success
+	}
+
+	pub fn set_high_cover(
+		&mut self,
+		rt: Option<RTCover>,
+	) -> WTOperation {
+		if let Some(_) = &self.structure {
+			return WTOperation::ObjectInWay;
+		}
+		for x in self.cover.iter_mut() {
+			if let None = x {
+				*x = rt;
+				return WTOperation::Success;
+			}
+		}
+		WTOperation::OutOfSpace
 	}
 }
