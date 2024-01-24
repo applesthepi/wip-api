dd89a04b2f0d36c7169a2f740c1a3ab88c94c560
Jackson Oliver <applesthepi@gmail.com>
2024-01-24 15:32:08 -0500

    entity body textures

--- src/generator/super_protocol/entity.rs
+++ src/generator/super_protocol/entity.rs
@@ -9,13 +9,38 @@ pub enum HumanBuild {
 	Bulk,
 }
 
-#[derive(Clone, Default)]
+impl HumanBuild {
+	pub fn as_prefix_str(
+		&self,
+	) -> &'static str {
+		match self {
+			HumanBuild::Small => "s",
+			HumanBuild::Medium => "m",
+			HumanBuild::Large => "l",
+			HumanBuild::Bulk => "b",
+		}
+	}
+}
+
+#[derive(Clone)]
 pub enum HumanAttire {
-	#[default]
 	Shoes,
 	Pants,
 	Shirt,
 	Vest,
+}
+
+impl HumanAttire {
+	pub fn as_str(
+		&self,
+	) -> &'static str {
+		match self {
+			HumanAttire::Shoes => "shoes",
+			HumanAttire::Pants => "pants",
+			HumanAttire::Shirt => "shirt",
+			HumanAttire::Vest => "vest",
+		}
+	}
 }
 
 #[derive(Clone, Default)]
@@ -35,9 +60,35 @@ pub struct RTEntityHuman {
 	pub attire: RTHumanAttire,
 }
 
+impl RTEntityHuman {
+	pub fn get_textures(
+		&self,
+	) -> (String, Vec<String>) {
+		let body = String::from_iter([
+			"human_",
+			self.build.as_prefix_str(),
+		]);
+		let mut extras = Vec::new();
+		if let Some(rt_ident) = &self.attire.shoes {
+
+		}
+		(body, extras)
+	}
+}
+
 #[derive(Clone)]
 pub enum RTEntityType {
 	Human(RTEntityHuman),
+}
+
+impl RTEntityType {
+	pub fn get_textures(
+		&self,
+	) -> (String, Vec<String>) {
+		match self {
+			RTEntityType::Human(rt_entity_human) => rt_entity_human.get_textures(),
+		}
+	}
 }
 
 #[derive(Clone)]
