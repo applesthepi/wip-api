c9a74ba391ef83b8c734beb4a26429bf3f90fa66
Jackson Oliver <applesthepi@gmail.com>
2024-02-07 17:12:11 -0500

    world entity identifier

--- src/world/commands.rs
+++ src/world/commands.rs
@@ -1,11 +1,11 @@
-use bevy::prelude::Event;
+use bevy::prelude::{Commands, Entity, Event};
 use wip_primal::{EntityPositionAbs, TilePositionAbs};
 
 use crate::{RTEntity, RTIdent};
 
 /// Proxy for submitting world commands cross system/api.
 pub struct WorldCommands<'w> {
-	pub spawning_criteria: &'w mut EntitySpawningCriteria,
+	// pub spawning_criteria: &'w mut EntitySpawningCriteria,
 	pub commands: &'w mut Vec<WorldCommand>,
 }
 
@@ -13,31 +13,27 @@ impl<'w> WorldCommands<'w> {
 	/// Register entity's existence.
 	pub fn entity_register(
 		&mut self,
+		commands: &mut Commands,
 		rt_entity: RTEntity,
 		tile_position_abs: TilePositionAbs,
-	) -> u32 {
-		let id = self.spawning_criteria.unused_ids.pop();
-		let id = match id {
-			Some(id) => id,
-			None => {
-				self.spawning_criteria.next_id += 1;
-				self.spawning_criteria.next_id
-			},
+	) -> WEIdent {
+		let we_ident = WEIdent {
+			world: commands.spawn_empty().id(),
+			ui: commands.spawn_empty().id(),
 		};
 		self.commands.push(
-			WorldCommand::WorldEntity(WorldEntityCommand::Register(id, rt_entity, tile_position_abs)),
+			WorldCommand::WorldEntity(WorldEntityCommand::Register(we_ident, rt_entity, tile_position_abs)),
 		);
-		id
+		we_ident
 	}
 
 	/// Destroys entity's existence.
 	pub fn entity_destroy(
 		&mut self,
-		id: u32,
-		faction_id: u32,
+		we_ident: WEIdent,
 	) {
 		self.commands.push(
-			WorldCommand::WorldEntity(WorldEntityCommand::Destroy(id, faction_id)),
+			WorldCommand::WorldEntity(WorldEntityCommand::Destroy(we_ident)),
 		);
 	}
 }
@@ -50,15 +46,22 @@ pub enum WorldCommand {
 #[derive(Event)]
 pub enum WorldEntityCommand {
 	/// Register entity's existence.
-	Register(u32, RTEntity, TilePositionAbs),
+	Register(WEIdent, RTEntity, TilePositionAbs),
 	/// Destroys entity's existence.
-	Destroy(u32, u32),
+	Destroy(WEIdent),
 }
 
-/// Defines how entities are spawned on a low level. Used
-/// to proxy information cross system/api.
-#[derive(Default)]
-pub struct EntitySpawningCriteria {
-	pub unused_ids: Vec<u32>,
-	pub next_id: u32,
+#[derive(Copy, Clone, PartialEq)]
+pub struct WEIdent {
+	pub world: Entity,
+	pub ui: Entity,
 }
+
+// Defines how entities are spawned on a low level. Used
+// to proxy information cross system/api.
+
+// #[derive(Default)]
+// pub struct EntitySpawningCriteria {
+// 	pub unused_ids: Vec<u32>,
+// 	pub next_id: u32,
+// }
--- src/world/pt/pt_chunk.rs
+++ src/world/pt/pt_chunk.rs
@@ -1,12 +1,12 @@
 use std::{rc::Rc, cell::{RefMut, RefCell, Ref}, borrow::BorrowMut, slice::from_ptr_range, sync::{Arc, RwLock}};
 
 use wip_primal::CHUNK_WIDTH;
-use crate::WorldTile;
+use crate::{WEIdent, WorldTile};
 
 #[derive(Default)]
 pub struct PhysicalChunk {
 	pub tiles: [[WorldTile; CHUNK_WIDTH as usize]; CHUNK_WIDTH as usize],
-	pub entities: Vec<u32>,
+	pub entities: Vec<WEIdent>,
 	pub dirty: bool,
 }
 
